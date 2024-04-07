9324de0c4604c7c6f6c75d2be9eba7f4e8c92178
mithun20522 <mithun20522@iiitd.ac.in>
2024-04-07 16:01:18 +0530

    header done

--- manage-state-context/src/App.jsx
+++ manage-state-context/src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Error from './pages/Error';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/products' element={<Products/>} />
+        <Route path='*' element={<Error/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ manage-state-context/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FaShoppingCart } from "react-icons/fa";
+const Header = () => {
+  return (
+    <header className='flex justify-between items-center p-5 bg-slate-500 text-white font-medium'>
+        <h1 className='text-2xl font-bold text-purple-300'>ECommerce </h1>
+        <ul className='flex gap-5'>
+            <li className='hover:underline cursor-pointer'>Home</li>
+            <li className='hover:underline cursor-pointer'>About</li>
+        </ul>
+        <FaShoppingCart size={30}/>
+    </header>
+  )
+}
+
+export default Header
